ff140687ba9abe0360efd2b4c6c50f9debcde8d5
ide127 <a71825485@gmail.com>
2019-10-23 02:39:27 +0900

    워드임베딩 작업 후 유사도 분석 기능 추가했습니다.

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask, request, session, redirect, url_for, abort, render_temp
 from flask_restful import reqparse, abort, Api, Resource
 from konlpy.tag import Kkma
 from konlpy.utils import pprint
+from __future__ import print_function
+from gensim.models import KeyedVectors
 import json
 
 app = Flask(__name__)
@@ -15,7 +17,7 @@ def hello_world():
 def post_action():
     if request.method == 'POST':
         #json 데이터를 받습니다.
-        json_data = request.get_json(); 
+        json_data = request.get_json()
         print('json_data : {json_data}')
 
         #꼬꼬마 객체를 생성
@@ -33,6 +35,30 @@ def post_action():
         #명사들을 return 해줍니다
         return nouns_json
     return 'none POST!'
+
+#유사도 분석을 수행합니다. 
+@app.route('/api/analyze-sentence', methods =['POST'])
+def similarity_analysis():
+    if request.method == 'POST':
+        #워드 임베딩 모델 파일을 가져옵니다. 절대경로로 지정되어 있으니 적절히 수정하세요.
+        ko_model = KeyedVectors.load_word2vec_format(r'C:\Users\Admin\Desktop\ko\ko.vec')
+        json_data = request.get_json()
+        
+        #의뢰받은 명사들을 추출합니다.
+        request_nouns = json_data['request_noun']
+        #어플이 디비에서 가지고 있는 모든 명사를 추출합니다.
+        total_nouns = json_data['total_nouns']
+
+        #return할 json 형식 데이터를 만듭니다.
+        first_dict = {}
+        second_dict = {}
+        for request_noun in request_noun:
+            for total_noun in total_nouns:
+                simil = ko_model.wv.similarity(request_noun,total_noun)
+                second_dict[total_noun] = simil
+            first_dict[request_noun] = second_dict
+        
+        return json.dumps(first_dict)
 
 
 if __name__ == '__main__':
